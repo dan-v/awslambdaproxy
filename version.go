9390d8cd45a9fa2c1c41f98dd743f943c7ca1290
Dan <1596016+dan-v@users.noreply.github.com>
2016-12-23 22:41:50 -0800

    more readme changes

--- version.go
+++ version.go
@@ -2,8 +2,7 @@ package awslambdaproxy
 
 import "strings"
 
-const Version = "0.0.1"
-
+const Version = "0.0.3"
 func LambdaVersion() string {
 	return "v" + strings.Replace(Version, ".", "-", -1)
 }
