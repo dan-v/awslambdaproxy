152da12d562113e0b17fa7b767fd05c058feae72
Mr Example <example@example.com>
2019-10-21 04:35:23 +1100

    more pronounced output

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,8 @@ terraform plan -out terraform.plan
 terraform apply terraform.plan
 IP=$(terraform output lambdaproxybox-ec2-ip)
 echo -e "\n\n"
-echo -e "To start proxy:\n\tssh ubuntu@$IP /home/ubuntu/awslambdaproxy run &"
+echo "####################### To start proxy: ########################"
+echo ""
+echo "        ssh ubuntu@$IP /home/ubuntu/awslambdaproxy run &        " 
+echo ""
+echo "################################################################"
