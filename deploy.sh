ef94a674cffaeccbfe96d977d95588498ff570ce
Mr Example <example@example.com>
2019-10-22 15:32:40 +1100

    only permit access to proxy via SSH tunnel

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
 cd terraform
+
 terraform init
 terraform plan -out terraform.plan
 terraform apply terraform.plan
 IP=$(terraform output lambdaproxybox-ec2-ip)
 EC2_USER=$(terraform output ec2_user)
-cat <<EOF
+ssh -o "StrictHostKeyChecking=no" -L 8080:localhost:8080 $EC2_USER@$IP ./awslambdaproxy run
 
-
-###################################### To start proxy: ########################################
-      ssh -o "StrictHostKeyChecking=no" $EC2_USER@$IP /home/$EC2_USER/awslambdaproxy run &
-###############################################################################################
-EOF
 cd -
--- terraform/aws.tf
+++ terraform/aws.tf
@@ -41,13 +41,6 @@ resource "aws_security_group" "lambdaproxy" {
   vpc_id = "${aws_vpc.lambdaproxy.id}"
 
   ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
