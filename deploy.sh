b2e5b4db3da2c6faa408ca78ceb287ca00ec86c8
Mr Example <example@example.com>
2019-10-22 15:10:41 +1100

    properly ssh in

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+cd terraform
 terraform init
 terraform plan -out terraform.plan
 terraform apply terraform.plan
@@ -8,7 +9,8 @@ EC2_USER=$(terraform output ec2_user)
 cat <<EOF
 
 
-########################## To start proxy: ##########################
-      ssh ubuntu@$IP /home/$EC2_USER/awslambdaproxy run &
-#####################################################################
+###################################### To start proxy: ########################################
+      ssh -o "StrictHostKeyChecking=no" $EC2_USER@$IP /home/$EC2_USER/awslambdaproxy run &
+###############################################################################################
 EOF
+cd -
